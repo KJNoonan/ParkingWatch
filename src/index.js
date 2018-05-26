ab4dddabbf76cb3e7cfaac3a1170277d77f3c05b
Kevin Noonan <KJNoonan52@gmail.com>
2018-05-26 11:21:42 -0500

    Added config to store the token

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { GET_VERB, runService } from "./service/HTTPFetcher";
 import { buildChicagoStreetClosureDataUrl } from "./service/ChicagoDataUrl";
 
 const address = "1600 W BELL PLAINE";
-//runService(GET_VERB, "https://google.com").then(console.log, console.log);
+runService(GET_VERB, "https://google.com").then(console.log, console.log);
